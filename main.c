c0f5cce0e3660dd951a61be0df924f818eadc759
Arpit Mishra <arpitmishra4779@gmail.com>
2018-12-02 00:23:08 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+double x0,y0,h,xf,n;
+void main()
+{
+    printf("Enter the vslue of x`0");
+    scanf("%lf",&x0);
+    printf("Enter the vslue of y`0");
+    scanf("%lf",&y0);
+    printf("Enter the vslue of x`f");
+    scanf("%lf",&xf);
+    printf("Enter the vslue of h");
+    scanf("%lf",&h);
+    n=(xf-x0)/h;
+
+
+}
